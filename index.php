b0b6c7be5067abbd6a9bf4d03886a18b84697aa1
arbi <arbi@arbi-debian.arbi.lan>
2015-09-08 11:43:23 +0200

    ajouter branche

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Test Git</title>
+        <title>Test Git Branche</title>
         <meta name="description" content="Test">
     </head>
     <body>
